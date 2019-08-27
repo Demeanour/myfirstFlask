b5d99cc9e7e5f70c7a6834b926f96e2634a951a6
Demeanour <tken.motsoari@gmail.com>
2019-08-27 13:59:54 +0200

    updated the server code for the 4th time

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, requests
+from flask import Flask, request
 from flask_restful import (reqparse, abort, Resource, Api)
+from flask_restful import fields, marshal_with
 
 app = Flask(__name__)
 api = Api(app)
 
+recource_fields = {
+    'tast': fields.String,
+    'url': fields.Url('tuneloft_ep')
+}
 
 ## Error handling functions
 ## if item doesn exist Send a message to notify
@@ -13,7 +18,7 @@ def abort_it_doesnt_exist(self,item):
     self.item = item
     abort(404, message="Item {} doesn't exist").format(item)
 
-def verify_account(self, email)
+def verify_account(self, email):
     ## send an email or text to verify account
     pass
 
@@ -21,6 +26,11 @@ def verify_account(self, email)
 ######### End of Error handling funcitons ###########
 
 #### User creation and login ####
+
+class Users(Resource):
+    def __init__(self):
+        pass
+
 class Signup(Resource):
     """
         When the user registers on to the service, they have 4 options:
@@ -34,7 +44,8 @@ class Signup(Resource):
     """
     def put(self, signup):
         pass
-    
+    def get(self,uid):
+        return {'this is ': "a test"}
 
 ## Class for logging in the user.
 class Login(Resource):
@@ -42,7 +53,8 @@ class Login(Resource):
         This is the login class. create a token_id with login. and get device mac_address.
     """
     def get():
-        pass
+        return 'Testing'
+
 
 
 ######## End of User Creation and Login #######
@@ -59,8 +71,8 @@ class Timeline(Resource):
         are user posts, events, and stories). Then also has to make a settings check for user preferences
         (has to be a function call to the database again.)
     """
-    def __init__(self):
-        return {'timeline': 'this is my timeline'}
+    def get(self):
+        return  ("get working now", 203)
 
 
 class Music:
@@ -71,17 +83,34 @@ class Music:
     def __init__(self):
         pass
 
-    def get_music(self,req_id,trackID):
-        def
-    def view
+    def get():
+        blog_post = api.model('Blog post',{
+            'category_id': fields.Integer(attribute='category_id'),
+            'name': fields.String(attribute=lambda x: x._private_name)
+        })
+        return blog_post
+
+    def view_artist():
+        pass
 
 
+class Following(Resource):
+    def __init__(self):
+        test = {"name": "This is a test"}
+        return test
+    def view_user():
+        pass
 
 ###### End of Data and Features ######
 
 ## api Resource for all the api requests in app.
-
 api.add_resource(Timeline, '/')
-api.add_resource(SignUp, '/r')
+api.add_resource(Signup, '/r')
 api.add_resource(Login,)
-api.add_resource(Following, 'myfollowing')
+api.add_resource(Following, '/myfollowing')
+api.add_resource(Music, '/music')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=2000)
+    
+    
